276a04ad89ae7230d442cc868586886611b970e5
amTrinh <maitrinh8828@gmail.com>
2023-04-18 09:51:50 +0700

    first commit

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author maitr
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        var frame = new Frame();
+        
+    }
+}
